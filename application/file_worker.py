995b23a42878a26ce7bef3a827ba6aa2b8eab42a
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-13 14:29:07 +0300

    test data filling. added write_file, load_from_file in file_worker.py

---
+++ application/file_worker.py
@@ -0,0 +1,16 @@
+import json
+from .logger import logging
+
+
+def write_to_file(data, filename):
+    with open(filename, 'w', encoding='utf-8') as file_json:
+        json.dump(data, file_json, ensure_ascii=False)
+    logging.info(f'Write data to {filename}')
+
+
+def load_from_file(filename):
+    with open(filename, encoding='utf-8') as file:
+        data = json.load(file)
+    logging.info(f'Read data from {filename}')
+    return print(data)
+# TO DO: pretty table output.
