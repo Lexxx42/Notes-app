e771e1f614d1b518ef22207448bd136f58c5010c
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-15 16:28:58 +0300

    tested sorted print by date

--- application/pretty_print.py
+++ application/pretty_print.py
@@ -15,4 +15,13 @@ def prettytable_print_all(data_notes):
         logging.exception(e)
 
 def prettytable_print_filter_date(data_notes):
-    print("desc date")
+    try:
+        x = PrettyTable()
+        x.field_names = list(data_notes["notes"][0].keys())
+        for item in data_notes["notes"]:
+            x.add_row([item[i] for i in x.field_names])
+        print(x)
+        logging.info('Notes printed as table in console.')
+    except TypeError as e:
+        print(e)
+        logging.exception(e)
