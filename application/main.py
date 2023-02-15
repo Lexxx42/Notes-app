d6fa0acbfa6ca65cb0ffad6ef8afa4da8119e324
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-15 16:36:52 +0300

    need to work on wait_for_continue() function

--- application/main.py
+++ application/main.py
@@ -25,6 +25,7 @@ def main_handler(operation_code):
             else:
                 data_from_file = load_from_file(generate_filename(file_name_valid))
             pt_print_all(data_from_file)
+            wait_for_continue()  # TODO: wait for user to respond.
         case 12:
             file_name_valid = ask_about_filename()
             if not file_name_valid:
@@ -32,3 +33,4 @@ def main_handler(operation_code):
             else:
                 data_from_file = load_from_file(generate_filename(file_name_valid))
             pt_print_filter_date(data_from_file)
+            wait_for_continue()
